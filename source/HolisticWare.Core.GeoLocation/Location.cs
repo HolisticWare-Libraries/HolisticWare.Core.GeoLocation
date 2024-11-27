f10ab1fab13e1b5ecb056c66c9cc513075de330f
moljac <mcvjetko@holisticware.net>
2024-11-27 09:40:46 +0100

    Location

---
+++ source/HolisticWare.Core.GeoLocation/Location.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Core.GeoLocation
+{
+    public partial class 
+                                        Location
+    {
+        public
+                                        Location
+                                        (
+                                        )
+        {
+            return;
+        }
+    }
+}
