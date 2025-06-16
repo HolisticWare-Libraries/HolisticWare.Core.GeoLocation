4f0143c02a980758ac52a00124ca3e50e66e5ca6
moljac <mcvjetko@holisticware.net>
2025-06-16 21:37:18 +0200

    Update Program.cs

--- samples/performance-benchmark-tests/AppConsole.Benchmarks.CSharp/Program.cs
+++ samples/performance-benchmark-tests/AppConsole.Benchmarks.CSharp/Program.cs
@@ -5,26 +5,43 @@ dotnet run \
         -c Release \
         -f net9.0
 
+dotnet run \
+    --project samples/performance-benchmark-tests/AppConsole.Benchmarks.CSharp/AppConsole.Benchmarks.CSharp.csproj \
+        -c Release \
+        -f net9.0
 */
-Benchmarks.Cvjetko_Milas.Dump();
-Benchmarks.Kombi.Dump();
-Benchmarks.Seat.Dump();
-Benchmarks.NathanRooy.Dump();
+
+Benchmarks.Earth.Distance.Calculate.Cvjetko_Milas.Dump();
+Benchmarks.Earth.Distance.Calculate.Kombi.Dump();
+Benchmarks.Earth.Distance.Calculate.Seat.Dump();
 Console.WriteLine("              Correct = 2.7161912585815897");
 
+Benchmarks.Earth.Distance.Calculate.NathanRooy.Dump();
+Benchmarks.Earth.Distance.Calculate.MladostTrackField100m.Dump();
+Benchmarks.Earth.Distance.Calculate.DinamoField105m.Dump();
+Benchmarks.Earth.Distance.Calculate.SveticeTrackField100m.Dump();
 
+
+
+/*
+*/
 BenchmarkDotNet.Reports.Summary summary = null;
 
 
 summary = BenchmarkDotNet.Running
-                           .BenchmarkRunner.Run<Benchmarks.Cvjetko_Milas>();
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.Cvjetko_Milas>();
 summary = BenchmarkDotNet.Running
-                           .BenchmarkRunner.Run<Benchmarks.Kombi>();
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.Kombi>();
 summary = BenchmarkDotNet.Running
-                           .BenchmarkRunner.Run<Benchmarks.Seat>();
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.NathanRooy>();
 summary = BenchmarkDotNet.Running
-                           .BenchmarkRunner.Run<Benchmarks.NathanRooy>();
-
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.Seat>();
+summary = BenchmarkDotNet.Running
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.MladostTrackField100m>();
+summary = BenchmarkDotNet.Running
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.DinamoField105m>();
+summary = BenchmarkDotNet.Running
+                           .BenchmarkRunner.Run<Benchmarks.Earth.Distance.Calculate.SveticeTrackField100m>();
 System.IO.File.Move
                 (
                     summary.LogFilePath,
